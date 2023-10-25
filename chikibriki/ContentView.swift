ed528f9a8b25249ec4914bce2c7186634e5c4974
dubsidiya <evapve@mail.ru>
2023-10-25 06:23:30 +0300

    Initial Commit

---
+++ chikibriki/ContentView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  chikibriki
+//
+//  Created by Vlad Kharin on 25.10.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Button{
+                //
+            }
+                    label: {
+                VStack{
+                    Spacer()
+                    Text("sqsqs")
+                    Spacer()
+                }
+                
+            }
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
---
+++ chikibriki/chikibrikiApp.swift
@@ -0,0 +1,17 @@
+//
+//  chikibrikiApp.swift
+//  chikibriki
+//
+//  Created by Vlad Kharin on 25.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct chikibrikiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
